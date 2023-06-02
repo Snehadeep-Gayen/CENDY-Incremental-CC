4bbae5e269a6514598e7138cd26513e52957e826
Snehadeep Gayen <cs21b078@smail.iitm.ac.in>
2023-06-03 02:42:45 +0530

    Added BFSplus class

--- Graph.h
+++ Graph.h
@@ -8,7 +8,7 @@ using namespace std;
 class Graph
 {
 public:
-    int v;  // no of vertices
+    int v; // no of vertices
     vector<vector<int>> adj;
 
     Graph();
@@ -16,6 +16,8 @@ public:
     void addVertex(vector<int> &neighboursList);
     void addEdge(int u1, int u2);
 };
+
+// Implementation
 
 Graph::Graph()
 {
@@ -25,7 +27,7 @@ Graph::Graph()
 
 void Graph::addVertex(vector<int> &neighbourList)
 {
-    for(auto i : neighbourList)
+    for (auto i : neighbourList)
         adj[i].push_back(v);
     v++;
     adj.push_back(neighbourList);
@@ -35,6 +37,66 @@ void Graph::addEdge(int u1, int u2)
 {
     adj[u1].push_back(u2);
     adj[u2].push_back(u1);
+}
+
+class BFSplus : public Graph
+{
+public:
+    vector<vector<int>> type;
+    vector<int> dist;
+
+    enum edgeType
+    {
+        TREE,
+        HORIZONTAL,
+        INTERLEVEL
+    };
+
+    BFSplus(int vertex, Graph& g);
+};
+
+BFSplus::BFSplus(int vertex, Graph& g)
+{
+    v = g.v;
+    adj = g.adj;
+
+    type = adj;
+    for(int i=0; i<v; i++)
+        for(auto &j : adj[i])
+            j=-1;
+
+    dist = vector<int>(v, -1);
+
+    queue<int> q;
+    q.push(vertex);
+    dist[vertex] = 0;
+    while(!q.empty())
+    {
+        for(int ii=0; ii<adj[q.front()].size(); ii++)
+        {
+            int i = adj[q.front()][ii];
+            if(dist[i]==-1)
+            {
+                type[q.front()][ii] = TREE;
+                dist[i] = dist[q.front()] + 1;
+                q.push(i);
+            }
+        }
+        q.pop();
+    }
+    
+    for(int i=0; i<v; i++)
+    {
+        for(int j=0; j<adj[i].size(); j++)
+        {
+            if(type[i][j]!=-1)
+                continue;
+            if(dist[i]==dist[adj[i][j]])
+                type[i][j] = HORIZONTAL;
+            else if(dist[i]!=dist[adj[i][j]])
+                type[i][j] = INTERLEVEL;
+        }
+    }
 }
 
 #endif
