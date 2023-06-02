66ba3a6215e6ed2906410b6a4c8e13c62a26bd8d
Snehadeep Gayen <cs21b078@smail.iitm.ac.in>
2023-06-03 02:00:43 +0530

    Graph class

--- Graph.h
+++ Graph.h
@@ -1,12 +1,40 @@
 #ifndef GRAPH_HEADER
 #define GRAPH_HEADER
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
 class Graph
 {
 public:
+    int v;  // no of vertices
+    vector<vector<int>> adj;
 
+    Graph();
 
-private:
+    void addVertex(vector<int> &neighboursList);
+    void addEdge(int u1, int u2);
 };
+
+Graph::Graph()
+{
+    adj = vector<vector<int>>();
+    v = 0;
+}
+
+void Graph::addVertex(vector<int> &neighbourList)
+{
+    for(auto i : neighbourList)
+        adj[i].push_back(v);
+    v++;
+    adj.push_back(neighbourList);
+}
+
+void Graph::addEdge(int u1, int u2)
+{
+    adj[u1].push_back(u2);
+    adj[u2].push_back(u1);
+}
 
 #endif
